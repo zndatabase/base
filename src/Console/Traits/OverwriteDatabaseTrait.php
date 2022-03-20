33e1699c49ea06f48358fb4eecf8efc5ab1ce1ec
ZnDeveloper <zndev@ya.ru>
2022-03-21 01:26:16 +0600

    upd

---
+++ src/Console/Traits/OverwriteDatabaseTrait.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ZnDatabase\Base\Console\Traits;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use ZnCore\Base\Libs\App\Helpers\ContainerHelper;
+use ZnCore\Base\Libs\DotEnv\DotEnv;
+use ZnDatabase\Base\Domain\Repositories\Eloquent\SchemaRepository;
+
+trait OverwriteDatabaseTrait
+{
+
+    protected function defaultExclideHosts(): array {
+        return [
+//            'localhost',
+//            '127.0.0.1',
+        ];
+    }
+
+    protected function defaultExclideDatabaseNames(): array {
+        return [
+            //'*_test',
+        ];
+    }
+    
+    protected function isContinue(InputInterface $input, OutputInterface $output): bool
+    {
+        /** @var SchemaRepository $schemaRepository */
+        $schemaRepository = ContainerHelper::getContainer()->get(SchemaRepository::class);
+        $connection = $schemaRepository->getConnection();
+        
+        $host = $connection->getConfig('host');
+        $exclude = DotEnv::get('DUMP_EXCLUDE_HOSTS', $this->defaultExclideHosts());
+        $isExclude = in_array($host, $exclude);
+        if ($isExclude) {
+            return true;
+        }
+
+        $database = $connection->getConfig('database');
+        $exclude = DotEnv::get('DUMP_EXCLUDE_DATABASES', $this->defaultExclideDatabaseNames());
+        foreach ($exclude as $ex) {
+            if ($ex == $database || fnmatch($ex, $database)) {
+                return true;
+            }
+        }
+
+        /*$isExclude = in_array($database, $exclude);
+        if($isExclude) {
+            return true;
+        }*/
+
+//        dd($this->dbRepository->getConnection()->getConfig('database'));
+
+        $output->writeln('');
+        $output->writeln('Further actions may overwrite your database!');
+        $question = new ConfirmationQuestion('Do you want to continue? (y|N): ', false);
+        $helper = $this->getHelper('question');
+        $isContinue = $helper->ask($input, $output, $question);
+        $output->writeln('');
+        return $isContinue;
+    }
+}
