12ed635bb688d161c08fe71baf918aa178f6ebce
Vitaliy <theyamshikov@yandex.ru>
2022-06-08 15:40:05 +0600

    upd

--- src/Domain/Helpers/ConfigHelper.php
+++ src/Domain/Helpers/ConfigHelper.php
@@ -2,14 +2,14 @@
 
 namespace ZnDatabase\Base\Domain\Helpers;
 
+use GuzzleHttp\Psr7\Uri;
 use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
-use ZnCore\Base\Libs\DotEnv\DotEnv;
 use ZnDatabase\Base\Domain\Enums\DbDriverEnum;
 
 class ConfigHelper
 {
 
-    public static function parseDsn($dsn)
+    public static function parseDsn(string $dsn): array
     {
         $dsnConfig = parse_url($dsn);
         $dsnConfig = array_map('rawurldecode', $dsnConfig);
@@ -23,24 +23,40 @@ class ConfigHelper
         return $connectionCofig;
     }
 
+    public static function generateDsn(array $connectionCofig): string
+    {
+        $uri = new Uri();
 
+// postgresql://myuser:mypassword@localhost:5634/lock
+        if ($connectionCofig['driver'] === DbDriverEnum::PGSQL) {
+//            dd($connectionCofig['database']);
+
+            $uri = $uri
+                ->withScheme('postgresql')
+                ->withHost($connectionCofig['host'])
+                ->withPort($connectionCofig['port'] ?? 5432)
+                ->withUserInfo($connectionCofig['username'], $connectionCofig['password'])
+                ->withPath('/' . $connectionCofig['database'])
+            ;
+        }
+
+//        dd($uri->__toString());
+
+        return $uri->__toString();
+    }
 
     public static function prepareConfig($connection)
     {
         if (!empty($connection['database'])) {
             $connection['database'] = rtrim($connection['database'], '/');
         }
-
         if (!empty($connection['read']['host'])) {
 
             $connection['read']['host'] = explode(',', $connection['read']['host']);
         }
-
         if (!empty($connection['write']['host'])) {
             $connection['write']['host'] = explode(',', $connection['write']['host']);
         }
-
         return $connection;
     }
-
 }
